9132029f2de8ccfe7eb0dc317a6b381079b084aa
Elias <elias0789@gmail.com>
2016-04-15 16:32:41 +0100

    Flesh out skeleton

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -1,7 +1,7 @@
 import Hapi from 'hapi'
 
 // helper methods
-import { handlePlugins } from './helpers/server-helpers.js'
+import {handlePlugins} from './helpers/server-helpers.js'
 
 // server plugins
 import Inert from 'inert'
@@ -12,12 +12,12 @@ import Images from './routes/Images.js'
 import ReactUrls from './routes/ReactUrls.js'
 import Scripts from './routes/Scripts.js'
 
-const Plugins = [ Inert ]
-const Routes = [ Hello, Images, ReactUrls, Scripts ]
+const Plugins = [Inert]
+const Routes = [Hello, Images, ReactUrls, Scripts]
 
 // Must export function that takes a two arguments: a config object, and the redis client
 // Function must return an UNSTARTED server object
-export default (client) => {
+export default () => {
 
   const server = new Hapi.Server()
 
--- frontend/src/js/components/Header/index.js
+++ frontend/src/js/components/Header/index.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
-import { Link } from 'react-router'
+import {Navbar, Nav} from 'react-bootstrap'
+import {Link} from 'react-router'
 
 export default class Header extends React.Component {
   constructor () {
     super()
-    this.state = { menuOpen: false }
+    this.state = {menuOpen: false}
   }
 
   render () {
     return (
       <Navbar
         expanded={this.state.menuOpen}
-        onToggle={() => { this.setState({ menuOpen: !this.state.menuOpen }) }}
+        onToggle={() => {this.setState({menuOpen: !this.state.menuOpen})}}
         className='top-menu'
-        fixedTop={true}>
+        fixedTop={true}
+                                                                                                                                                                                                                                                                            >
         <Navbar.Header>
           <Navbar.Brand>
             <Link to={'/'}>
@@ -27,9 +28,10 @@ export default class Header extends React.Component {
           <Nav pullRight>
             {this.props.menuItems.map(item => {
               return (
-                <li onClick={() => { this.setState({ menuOpen: false }) }}
-                    role='presentation'
-                    key={item + '-li'}>
+                <li onClick={() => {this.setState({menuOpen: false})}}
+                  role='presentation'
+                  key={item + '-li'}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        >
                   <Link key={item} to={'/' + item}>{item}</Link>
                 </li>
               )
