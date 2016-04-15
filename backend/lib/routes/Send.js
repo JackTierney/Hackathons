1dbba7399aa180fd4e80d2431f9b939dfe410dec
Elias <elias0789@gmail.com>
2016-04-15 21:50:16 +0100

    correct formatting

--- backend/lib/routes/Send.js
+++ backend/lib/routes/Send.js
@@ -1,18 +1,18 @@
-import zensend  from 'zensend'
+import zensend from 'zensend'
 
-	var client = new zensend.Client("Tw40TCl18EDda8eLW5hxMw");
+const client = new zensend.Client('Tw40TCl18EDda8eLW5hxMw')
 
-
-
-  export default {
-    path: '/sendSms',
-    method: 'GET',
-    handler: (response, reply) => {
-      client.sendSms({
-    	  originator: "SPINTOWIN",
-    	  body: "Hello Jack, Elias and Taz, this is a test message",
-    	  numbers: ["447732955369","447415583356","447789623006"]
-    	}, function(error, response) {});
-
-    }
+export default {
+  path: '/sendSms',
+  method: 'GET',
+  handler: (req, reply) => {
+    client.sendSms({
+      originator: 'SPINTOWIN',
+      body: 'Hello Jack, Elias and Taz, this is a test message',
+      numbers: ['447732955369', '447415583356', '447789623006']
+    }, (error, res) => {
+      if (error) return reply({success: false, data: error})
+      return reply({success: true, data: res})
+    })
   }
+}
