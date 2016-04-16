ae0b2a676dba852d04e70abb1e10e5b17e55b5e5
tas12 <tsultanah89@gmail.com>
2016-04-16 16:39:31 +0100

    correct function args

--- backend/lib/message/index.js
+++ backend/lib/message/index.js
@@ -2,7 +2,7 @@ import zensend  from 'zensend'
 
 var client = new zensend.Client("Tw40TCl18EDda8eLW5hxMw");
 
-export default function sendFormLink (numbers, body) => {
+export default function sendFormLink (number, body) => {
 	client.sendSms({
 	  originator: "FAC",
 	  body: body,
