2fb1226741141ae8c10f912e63b5f12d89a02c44
tas12 <tsultanah89@gmail.com>
2016-04-16 18:41:00 +0100

    add scheduler function

--- backend/lib/scheduler/index.js
+++ backend/lib/scheduler/index.js
@@ -1,21 +1,33 @@
 import pg from 'pg'
 import sendFormLink from '../message/index.js'
+import schedule from 'node-schedule'
 
 const connectionDetails = {username: process.env.DBUSER, password: process.env.DBPWD, database: process.env.DB}
-const branchId = 1
+
 const query = {
-	text: 'SELECT first_name, phone FROM contacts WHERE branch_id = $1',
-	values: [branchId]
+	text: 'SELECT branch_id, first_name, phone FROM contacts WHERE branch_id IN (
+SELECT branch_id from branch_services WHERE branch_services.last_updated < CURRENT_TIMESTAMP - INTERVAL '1 days')',
 }
+
+var messageScheduler = schedule.scheduleJob('* * 10 * *', function(){
+  pg.connect(connectionDetails, function(err, client, done) {
+    if(err) {
+    	return console.error('error fetching client from pool', err);
+  	}
+    client.query(query, function(err, result) {
+  		result.rows.forEach(function (rowObj) {
+  			var url = process.env.HOST + '/update/' + rowObj.branch_id
+  			var body = 'Hello, '+ rowObj.first_name +'. Please click the link to update your service details.' + url
+  			sendFormLink(rowObj.phone, body)
+  	})
+  })
+});
+
+
 
 pg.connect(connectionDetails, function(err, client, done){
 	if(err) {
   	return console.error('error fetching client from pool', err);
 	}
-	client.query(query, function(err, result) {
-		result.rows.forEach(function (rowObj) {
-			var url = process.env.HOST + '/update/' + branchId
-			var body = 'Hello, '+ rowObj.first_name +'. Please click the link to update your service details.' + url
-			sendFormLink(rowObj.phone, body)
-	})
+
 })
