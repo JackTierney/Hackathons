f129edbaa3952883f7705e4d0d30edb8888516f4
Elias <elias0789@gmail.com>
2016-04-17 00:16:48 +0100

    refactor scheduler to use the pg client and export a function that sets up the scheduled job

--- backend/lib/scheduler/index.js
+++ backend/lib/scheduler/index.js
@@ -1,32 +1,30 @@
-import pg from 'pg'
+import pg from '../psql/client.js'
 import sendFormLink from '../message/index.js'
 import schedule from 'node-schedule'
 
-const connectionDetails = {username: process.env.DBUSER, password: process.env.DBPWD, database: process.env.DB}
+const HOST = process.env.HOST || 'http://localhost'
 
-const query = {
-	text: "SELECT branch_id, first_name, phone FROM contacts WHERE branch_id IN (SELECT branch_id from branch_services WHERE branch_services.last_updated < CURRENT_TIMESTAMP - INTERVAL '1 days')"
-}
-
-var messageScheduler = schedule.scheduleJob('* * 10 * *', function(){
-  pg.connect(connectionDetails, function(err, client, done) {
-    if(err) {
-    	return console.error('error fetching client from pool', err);
-  	}
-    client.query(query, function(err, result) {
-  		result.rows.forEach(function (rowObj) {
-  			var url = process.env.HOST + '/update/' + rowObj.branch_id
-  			var body = 'Hello, '+ rowObj.first_name +'. Please click the link to update your service details.' + url
-  			sendFormLink(rowObj.phone, body)
-  	})
+export default () => {
+  schedule.scheduleJob('16 0 * * *', () => {
+    pg((err, client, done) => {
+      if (err) return console.error('error fetching client from pool', err)
+      client.query(
+        'SELECT branch_id, first_name, phone FROM contacts ' +
+        'WHERE branch_id IN ' +
+        '(SELECT branch_id from branch_services ' +
+        'WHERE branch_services.last_updated < CURRENT_TIMESTAMP - INTERVAL \'12 hours\')',
+        (error, result) => {
+          if (error) throw error
+          result.rows.forEach((rowObj) => {
+            var url = HOST + '/update/' + rowObj.branch_id
+            var body = 'Hello, ' +
+              rowObj.first_name +
+              '. Please click the link to update your service details.' + url
+            sendFormLink(rowObj.phone, body)
+          })
+          done()
+        }
+      )
+    })
   })
-});
-
-
-
-pg.connect(connectionDetails, function(err, client, done){
-	if(err) {
-  	return console.error('error fetching client from pool', err);
-	}
-
-})
+}
