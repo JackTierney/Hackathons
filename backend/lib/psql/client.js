8b92abd000aaa266c4bae83332b507a22ae5c996
Elias <elias0789@gmail.com>
2016-04-17 11:36:12 +0100

    update pg connection object

--- backend/lib/psql/client.js
+++ backend/lib/psql/client.js
@@ -1,8 +1,11 @@
 import pg from 'pg'
 
 const connection = {
-  user: 'elias',
-  database: 'hsa'
+  user: process.env.DB_USER || 'elias',
+  database: process.env.DB_NAME || 'hsa',
+  password: process.env.DB_PASSWORD || null,
+  port: process.env.DB_PORT || 5432,
+  host: process.env.DB_HOST || 'localhost',
 }
 
 export default (cb) => pg.connect(connection, cb)
