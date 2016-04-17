b0cb630ea9a145552550628596cbb02aab6b32f1
Elias <elias0789@gmail.com>
2016-04-17 13:01:40 +0100

    fix

--- frontend/src/js/components/Form/index.js
+++ frontend/src/js/components/Form/index.js
@@ -1,65 +1,52 @@
 import React from 'react'
-import {Input, ButtonInput, Grid, Col, Row, ButtonGroup, DropdownButton, MenuItem} from 'react-bootstrap'
+import {
+  Input,
+  ButtonInput,
+  Grid,
+  Row,
+  ButtonGroup,
+} from 'react-bootstrap'
+import axios from 'axios'
 
-// console.log(document.getElementById('Provider').value, document.getElementById('Beds').value,
-// document.getElementById('Food').value, "wowowoooooooooooooooo")
-
-export default (props) => {
+export default () => {
   const handler = (e) => {
-    console.log('EVENT', e)
     e.preventDefault()
-    const id = window.location.pathname.split('/');
-    axios.post('/api/branch/'+ id[0],
-    {
+    const id = window.location.pathname.split('/')
+    axios.post('/api/branch/'+ id[0], {
       provider: document.getElementById('Provider').value,
-      beds: document.getElementById('Beds').value,
-      foods: document.getElementById('Food').value
+      beds: document.getElementById('Beds').value + 0,
+      servingFood: !! document.getElementById('Food').value
     })
-    .then(function (response) {
-      console.log(response);
+    .then((response) => {
+      console.log(response)
     })
-    .catch(function (response) {
-      console.log(response);
-    });
+    .catch((response) => {
+      console.log(response)
+    })
   }
-
-// const stylespan =
-// {
-//   .textcolor: {color:'#fff'}
-//
-// }
 
   return (
     <Grid>
       <Row>
         <form onSubmit={handler}>
-          <Input type="text" className="textcolor"label="Provider" id="Provider" placeholder="Enter text" />
-          <Input type="email" label="Contact Email Address" placeholder="Enter email" />
-          <Input type="phoneNumber" label="Contact Landline" placeholder="Enter Landline"/>
-          <Input type="phoneNumber" label="Contact Mobile" placeholder="Enter Landline"/>
-          <Input type="text" label="Are you serving food?" id ="Food" placeholder="Yes/No"/>
-          <Input type="number" label="Please enter number of beds" id ="Beds" placeholder="0-100"/>
-          <ButtonGroup label="Gender">
-            <Input type="radio" label="Male"  />
-            <Input type="radio" label="Female" />
-            <Input type="radio" label="Other" />
+          <Input type='text' className='textcolor' label='Provider' id='Provider'
+            placeholder='Enter text'
+          />
+          <Input type='email' label='Contact Email Address' placeholder='Enter email' />
+          <Input type='phoneNumber' label='Contact Landline' placeholder='Enter Landline'/>
+          <Input type='phoneNumber' label='Contact Mobile' placeholder='Enter Landline'/>
+          <Input type='text' label='Are you serving food?' id='Food' placeholder='Yes/No'/>
+          <Input type='number' label='Please enter number of beds' id='Beds' placeholder='0-100'/>
+          <ButtonGroup label='Gender'>
+            <Input type='radio' label='Male' />
+            <Input type='radio' label='Female' />
+            <Input type='radio' label='Other' />
           </ButtonGroup>
-          <Input type="file" label="Image" help="[Optional]" />
-          <Input type="textarea" label="Text Area" placeholder="textarea" />
-          <ButtonInput type="submit" value="Submit Button" />
+          <Input type='file' label='Image' help='[Optional]' />
+          <Input type='textarea' label='Text Area' placeholder='textarea' />
+          <ButtonInput type='submit' value='Submit Button' />
         </form>
       </Row>
     </Grid>
   )
 }
-
-
-// <DropdownButton title="Beds" id="bg-nested-dropdown">
-//   <MenuItem eventKey="1">0</MenuItem>
-//   <MenuItem eventKey="2">0-5</MenuItem>
-//   <MenuItem eventKey="3">5-10</MenuItem>
-//   <MenuItem eventKey="4">15-10</MenuItem>
-//   <MenuItem eventKey="5">20+</MenuItem>
-// </DropdownButton>
-// <Input type="Serving food" label="Select" id="Food" placeholder="select">
-// </Input>
