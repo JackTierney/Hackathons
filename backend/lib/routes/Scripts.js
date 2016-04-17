a3781da6ba25b8d4c0e140bc1eb1b46cb228724f
Elias <elias0789@gmail.com>
2016-04-17 12:16:10 +0100

    more logs

--- backend/lib/routes/Scripts.js
+++ backend/lib/routes/Scripts.js
@@ -1,11 +1,15 @@
 import path from 'path'
+import fs from 'fs'
 
 export default {
   path: '/{filename}.js',
   method: 'GET',
   handler: (req, reply) => {
     const p = path.join(__dirname, '..', '..', '..', 'public', req.path)
+    const stats = fs.statSync(p)
     console.log('This is my P: ', p)
+    console.log('P is a file?: ', stats.isFile())
+    console.log('P is a dir?: ', stats.isDirectory())
     reply.file(p)
   }
 }
